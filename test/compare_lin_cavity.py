6aa2cfd71c91373892066b4860b3896e3e39c2a6
Manas Rachh <mrachh@flatironinstitute.org>
2023-04-26 10:21:37 -0400

    updating plotting scripts

--- test/compare_lin_cavity.py
+++ test/compare_lin_cavity.py
@@ -26,6 +26,7 @@ cc = ['darkgreen','orangered','navy','magenta','black','cyan','green','yellow','
 zkvals = [37.213]
 nzk = size(zkvals)
 
+rsc = 40.61   # norm of density, computed separately
 figure(1)
 clf()
 
@@ -35,13 +36,13 @@ for i in range(nzk):
     mask = abs(zks-zkvals[i])<1e-5
     noverkuse = noverk[mask]
     ppwuse = ppw[mask]
-    errduse = errd[mask]
+    errduse = errd[mask]/rsc
     errquse = errq[mask]
 
     mask2 = abs(zks2-zkvals[i])<1e-5
     noverkuse2 = noverk2[mask2]
     ppwuse2 = ppw2[mask2]
-    errduse2 = errd2[mask2]
+    errduse2 = errd2[mask2]/rsc
     errquse2 = errq2[mask2]
     figure(1)
     semilogy(ppwuse,errduse,'.',color=cc[2*i])
@@ -53,11 +54,13 @@ for i in range(nzk):
 figure(1)    
 legend(['Const','Linear'])
 xlabel('Points per wavelength')
-ylabel(r'$|\sigma-\sigma_{n}|$')
+ylabel(r'$|\sigma-\sigma_{n}|$/$|\sigma|$')
+xlim([0,50])
 savefig('err_dens_cavity_dir_pw_mpi2p0p2_lincomp.pdf',bbox_inches='tight')
 figure(2)    
 legend(['Const','Linear'])
 xlabel('Points per wavelength')
 ylabel(r'$|\sigma-\sigma_{n}|/|(I-P_{N})\sigma|$')
+xlim([0,50])
 savefig('err_cq_cavity_dir_pw_mpi2p0p2_lincomp.pdf',bbox_inches='tight')
 show()
--- test/est_sig_norm.py
+++ test/est_sig_norm.py
@@ -4,15 +4,12 @@ from scipy.io import FortranFile
 from scipy.special import *
 
 ik = 6
-nch = 3269
-fname = 'cavity_data/sol_ik'+str(ik).zfill(1)+'_nch'+str(nch).zfill(5)+'_pw_mpi2p0p2.bin'
-nch = 1635
+nch = 2024
 fname = 'cavity_data/sol_ik'+str(ik).zfill(2)+'_nch'+str(nch).zfill(5)+'_pw_mpi2p0p2_lin.bin'
 fref_tot = 'cavity_data/fields_ref_utot_ik'+str(ik)+'_nch'+str(nch).zfill(5)+'_pw_mpi2p0p2.pdf'
 fref_sc = 'cavity_data/fields_ref_usc_ik'+str(ik)+'_nch'+str(nch).zfill(5)+'_pw_mpi2p0p2.pdf'
 fnord1_tot = 'cavity_data/fields_nord1_utot_ik'+str(ik)+'_nch'+str(nch).zfill(5)+'_pw_mpi2p0p2.pdf'
 fnord1_sc = 'cavity_data/fields_nord1_usc_ik'+str(ik)+'_nch'+str(nch).zfill(5)+'_pw_mpi2p0p2.pdf'
-ferr = 'cavity_data/fields_error_ik'+str(ik)+'_nch'+str(nch).zfill(5)+'_pw_mpi2p0p2.pdf'
 f = FortranFile(fname,'r')
 zk = f.read_record(complex)
 nch = int(f.read_ints(int32))
@@ -32,6 +29,7 @@ pottarg_plot_nord1 = f.read_record(complex)
 soln_use = f.read_record(complex)
 pottarg_plot_ref = f.read_record(complex)
 
+
 fname2 = 'cavity_data/cavity_interior.bin'
 g = FortranFile(fname2,'r')
 ntlat = int(g.read_ints(int32))
@@ -40,69 +38,7 @@ targs = g.read_reals(float).reshape((2,ntarg),order="F")
 isin = g.read_ints(int32)
 qwts = g.read_reals(float)
 
-print(ntlat)
-x = loadtxt('fort.37')
-
-s0 = 1.0/sqrt(abs(zk))
-thet = 0.45*pi
-ttuse = 3.91322070806951
-
-thet = -pi/2 + 0.2 
-
-uinc = zeros(size(targs[0,:]))
-ct = cos(thet)
-st = sin(thet)
-uinc = exp(1j*zk[0]*(targs[0,:]*ct + targs[1,:]*st))
-
-xt = targs[0,:]
-yt = targs[1,:]
-
-
-usc = uinc - pottarg_plot_ref
-usc0 = usc
-uplot = abs(usc)
-uplot[isin>-0.5] = NaN
-
-umax = max(uplot)
-uplot = uplot.reshape(ntlat,ntlat,order="F")
-
-figure(1)
-clf
-imshow(uplot,extent=[-4,4,-4,4],vmin=0,vmax=80)
-#plot(x[:,0],x[:,1],'w.')
-cb = colorbar(shrink=.9)
-savefig(fref_sc,bbox_inches='tight')
-
-
-usc = uinc - pottarg_plot_nord1
-usc1 = usc
-uplot = abs(usc)
-uplot[isin>-0.5] = NaN
-uplot = uplot.reshape(ntlat,ntlat,order="F")
-
-figure(2)
-clf
-imshow(uplot,extent=[-4,4,-4,4],vmin=0,vmax=80)
-#plot(x[:,0],x[:,1],'w.')
-cb = colorbar(shrink=.9)
-savefig(fnord1_sc,bbox_inches='tight')
-
-
-figure(3)
-clf
-uplot = log10(abs(usc0-usc1)/umax)
-uerrmax = max(uplot)
-print(uerrmax)
-uplot[isin>-0.5] = NaN
-uplot = uplot.reshape(ntlat,ntlat,order="F")
-
-imshow(uplot,extent=[-4,4,-4,4],vmin=-10,vmax=0)
-#plot(x[:,0],x[:,1],'w.')
-cb = colorbar(shrink=.9)
-savefig(fnord1_sc,bbox_inches='tight')
-
-
-show()
-
-
-
+snorm = sqrt(dot(abs(soln_use)**2,qwts))
+slen = sum(qwts)
+print(snorm)
+print(slen)
